89b4ec6723d8ddc323852d8584925f8ad06c95a8
Prince Kumar <princebantikumar@gmail.com>
2020-02-24 17:38:44 +0530

    final changes after learnig git reset commitID

--- learning.cpp
+++ learning.cpp
@@ -1,6 +1,11 @@
+//This is the first c++ program for learning git
 #include<iostream>
 int main(){
-	int a, b;
-	cout<<a<<b;
+	int a, b, result;
+	cout<<"Enter the two numbers: ";
+	cin>>a>>b;
+	result = a + b;
+	cout<<"Result : "<<result<<endl;
+	cout<<"Program completed";
 	return 0;
 }
